9069e4c1828c18d31c2b424f6eaf870cf3b8453f
Juha Heinanen <jh@tutpro.com>
2018-06-17 08:33:32 +0300

    better to use zrtp_status_bad_param error value

--- src/zrtp_protocol.c
+++ src/zrtp_protocol.c
@@ -255,7 +255,7 @@ zrtp_status_t _zrtp_protocol_decrypt( zrtp_protocol_t *proto,
 	zrtp_status_t s = zrtp_status_ok;
 
 	if (!proto)
-		return zrtp_error_software;
+		return zrtp_status_bad_param;
 
 	if (is_rtp) {
 		s = zrtp_srtp_unprotect(proto->context->zrtp->srtp_global, proto->_srtp, packet);
